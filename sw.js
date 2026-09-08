d5611d35161ac2fbdf723f62acd510853ebc9c96
ArbeenaMushtaq <arbeena12mushtaq@gmail.com>
2026-09-08 17:30:21 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache the app shell so Ledger opens with no connection at all.
-const CACHE = 'ledger-v8';
+const CACHE = 'ledger-v9';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
